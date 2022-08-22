44cc29c09fe0218fece33663d39f2bef5dadfc36
Jorge Torres <jorge.tm.ink@gmail.com>
2022-08-22 20:14:20 +0200

    Desarrollo Ejercicios sesión 3

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿Client client1 = new Client("Jorge", 612312312, "Av siempre viva", "qweasdas@asd.com", true);
+Console.WriteLine(client1);
+
+
+public struct Client
+{
+    public Client(string name, int phone, string address, string email, bool isNewClient )
+    {
+        Name = name;
+        Phone = phone;
+        Address = address;
+        Email = email;
+        IsNewClient = isNewClient;
+    }
+
+    public string Name { get; }
+    public int Phone { get; }
+    public string Address { get; }
+    public string Email { get; }
+    public bool IsNewClient { get; }
+
+    public override string ToString() => $@"
+    Nombre: {Name} 
+    Tel: {Phone} 
+    Direccion: {Address} 
+    Email: {Email} 
+    Cliente nuevo: {IsNewClient}
+    ";
+}
